e98105289525909f1c08cf8a17a4a03f5c3466c3
joseMigueel <josemiguemarre99@gmail.com>
2021-01-27 11:17:46 +0000

    Añadimos ya como queremos que se llame nuestro videojuego. Siguiente paso seleccionar fondo de nuestro juego

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pygame
 import sys
 import random
 
+pygame.init()
+
 pantalla_x = 600
 pantalla_y = 400
+pygame.display.set_caption('ASTERMINATOR')
+size =(pantalla_x,pantalla_y)
 
 pygame.display.flip()
