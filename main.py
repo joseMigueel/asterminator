b8205a1c93ec9b00d54433447833ffceb3ee62ad
joseMigueel <josemiguemarre99@gmail.com>
2021-01-27 12:01:15 +0000

    ya tenemos la primera parte del juego, coonseguimos ver el fondo de pantalla de la tierra.por ahora nuestro codigo corre sin dar error

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pygame 
 import sys
-import random
+
 
 pygame.init()
-
 pantalla_x = 600
 pantalla_y = 400
 pygame.display.set_caption('ASTERMINATOR')
 size =(pantalla_x,pantalla_y)
 screen = pygame.display.set_mode(size)
-background = pygame.image.load('img/backgroun_espacio').convert()
-pygame.display.flip()
+background = pygame.image.load('img//background_tierra.jpg').convert()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    screen.blit(background,[0,0])
+    pygame.display.flip()
