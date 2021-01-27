9896813b78d03b56db24bf2c85b23b599143eae0
joseMigueel <josemiguemarre99@gmail.com>
2021-01-27 11:33:08 +0000

    añadimos un while, lo que hara sera repetir el codigo todo el tiempo

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ pantalla_y = 400
 pygame.display.set_caption('ASTERMINATOR')
 size =(pantalla_x,pantalla_y)
 screen = pygame.display.set_mode(size)
-
+background = pygame.image.load('img/backgroun_espacio').convert()
 pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
