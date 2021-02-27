ed74ad3e933a45c86f1bfb704326c3a2ec59910f
joseMigueel <josemiguemarre99@gmail.com>
2021-02-27 18:25:08 +0000

    hemos actualizado la base de datos

--- modules/base_de_datos.py
+++ modules/base_de_datos.py
@@ -8,10 +8,12 @@ class DB():
 
         self.con = sqlite3.connect('Asterminator.db')
         self.cur = self.con.cursor()
-        self.con = sqlite3.connect(':Puntuaciones')
-        print('base_de_datos_creada')
+        
+        
         try: 
+            print('base_de_datos_creada')
             self.cur.execute('''CREATE TABLE puntuaciones(id INTEGER PRIMARY KEY AUTOINCREMENT,puntos integer,nombre VARCRCHAR (100)) ''')
+            self.cur.execute('''CREATE TABLE nombres(id INTEGER PRIMARY AUTOINCREMENT,nombre VARCRCHAR(50))''')
             self.con.commit()
         except:
             print('error') 
@@ -20,5 +22,9 @@ class DB():
         #self.cur.execute(consulta)
         #self.mi_conexion.commit()
         self.cur.execute('''INSERT INTO puntuaciones(puntos, nombre)VALUES(?,?)''',consulta)
+        self.con.commit()
+
+    def insert_name(self,consulta):
+        self.cur.execute('''INSERT INTO nombres(nombre)VALUES(?)''',consulta)
         self.con.commit()
         
