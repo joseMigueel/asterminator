283e4185a53956ff6c2b5b3f7ce8b1bb31c806d3
joseMigueel <josemiguemarre99@gmail.com>
2021-02-27 18:21:19 +0000

    hemos actualizado el menu, para añadir ver puntuacion del jugador

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_menu
+from modules import base_de_datos
 
 pygame.init()
 
@@ -7,19 +8,24 @@ surface = pygame.display.set_mode((600,300))
 
 
 def start_game():
+    db = base_de_datos.DB()
+    db.insert_score((100,"jose"))
     import main
 
 
 def set_dificultad(value, dificultad):
     pass
 
+def start_p():
+    import puntos
+    
+
 tema = pygame_menu.themes.THEME_GREEN
 tema.widget_font = pygame_menu.font.FONT_8BIT
 
 menu = pygame_menu.Menu(300,600,'Asterminator',theme=tema)
 menu.add_text_input('nombre: ',default='')
-
-menu.add_selector('dificultad: ',[('Dificil',1),('Facil',2)], onchange=set_dificultad )
+menu.add_button("ver puntuacion",start_p)
 menu.add_button('inicio',start_game)
 menu.add_button('salir',pygame_menu.events.EXIT)
 menu.mainloop(surface)
